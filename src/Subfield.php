4ce6f03258689672761b722792f8d63f84976079
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-12-19 15:04:18 +0000

    initial release

---
+++ src/Subfield.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Formfeed\Subfields;
+
+use Laravel\Nova\Fields\FieldCollection;
+
+interface Subfield
+{
+    
+    public function hasSubfields(): bool;
+
+    public function getSubfields(): FieldCollection;
+
+}
